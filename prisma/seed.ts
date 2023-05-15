fb15c6482011ca3d586dba555e73f7cdd8d64846
oscarfgmaia <oscarfgmaia@gmail.com>
2023-05-15 17:47:31 -0300

    add 1 year from now to seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -11,7 +11,7 @@ async function main() {
         logoImageUrl: 'https://files.driveneducation.com.br/images/logo-rounded.png',
         backgroundImageUrl: 'linear-gradient(to right, #FA4098, #FFD77F)',
         startsAt: dayjs().toDate(),
-        endsAt: dayjs().add(21, 'days').toDate(),
+        endsAt: dayjs().add(365, 'days').toDate(),
       },
     });
   }
