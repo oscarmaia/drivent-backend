38b0a1e2d4f79333e2d4028a62df20318920bb3a
oscarfgmaia <oscarfgmaia@gmail.com>
2023-05-15 17:47:48 -0300

    style: change quotes

--- src/routers/users-router.ts
+++ src/routers/users-router.ts
@@ -1,11 +1,11 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import { createUserSchema } from "@/schemas";
-import { validateBody } from "@/middlewares";
-import { usersPost,loginWithGithub } from "@/controllers";
+import { createUserSchema } from '@/schemas';
+import { validateBody } from '@/middlewares';
+import { usersPost, loginWithGithub } from '@/controllers';
 
 const usersRouter = Router();
 
-usersRouter.post("/", validateBody(createUserSchema), usersPost).post('/register/github',loginWithGithub);
+usersRouter.post('/', validateBody(createUserSchema), usersPost).post('/register/github', loginWithGithub);
 
 export { usersRouter };
